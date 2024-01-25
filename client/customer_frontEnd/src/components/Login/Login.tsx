13306820987d273fd224687e8b269ad05fbce475
maheshdila <maheshdilshan716@gmail.com>
2024-01-25 16:33:50 +0530

    login_page_done

---
+++ client/customer_frontEnd/src/components/Login/Login.tsx
@@ -0,0 +1,53 @@
+import {Link} from "react-router-dom";
+import "./additional.css";
+
+function Login () {
+    return (
+
+        <div className="loginBody">
+            <div className="wrapper">
+                <div className="form-box login">
+                    <h2>Login</h2>
+                    <form action="src/components">
+                        <div className="input-box">
+              <span className="icon">
+
+              </span>
+                            <input type="email"/>
+                            <label htmlFor="">email</label>
+                        </div>
+                        <div className="input-box">
+
+                            <input type="password"/>
+                            <label htmlFor="">Password</label>
+                        </div>
+
+                        <div className="remember">
+                            <label htmlFor="">
+                                <input type="checkbox"/>I agree to terms and conditions
+                            </label>
+                        </div>
+
+                        <button type="button" className="button_1">
+                            Login
+                        </button>
+
+                        <div className="login-register">
+                            <p>
+                                Don't have an account?{" "}
+                                <Link to="" className="register-link">
+                                    Register
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+
+
+export default Login;
