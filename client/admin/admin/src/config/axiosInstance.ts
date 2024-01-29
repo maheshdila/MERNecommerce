7ada75361458f88140c81c98ab3e62d7996a37c1
maheshdila <maheshdilshan716@gmail.com>
2024-01-29 10:12:39 +0530

    axios_interceptor_configurations

--- client/admin/admin/src/config/axiosInstance.ts
+++ client/admin/admin/src/config/axiosInstance.ts
@@ -19,13 +19,16 @@ instance.interceptors.request.use(
 );
 
 export default instance;*/
+/*
 import axios, { AxiosInstance } from "axios";
 import BASE_URL from '../config/ApiConfig.ts';
 
 const instance: AxiosInstance = axios.create({
     baseURL: BASE_URL
 });
+*/
 
+/*
 instance.interceptors.request.use(
     (config) => {
         // Get the token from the cookie
@@ -45,3 +48,46 @@ instance.interceptors.request.use(
 );
 
 export default instance;
+*/
+
+
+import axios from 'axios';
+import BASE_URL from '../config/ApiConfig.ts'
+
+// Set up Axios instance
+const axiosInstance = axios.create({
+        baseURL:BASE_URL
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+    (config) => {
+            // Get the token from cookies
+            const token = getCookie('token');
+
+            // Add the token to the request headers
+            if (token) {
+                    config.headers['Authorization'] = `Bearer ${token}`;
+            }
+
+            return config;
+    },
+    (error) => {
+            return Promise.reject(error);
+    }
+);
+
+// Function to get a cookie by name
+function getCookie(name) {
+        const cookieArray = document.cookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+                const cookiePair = cookieArray[i].split('=');
+                const cookieKey = cookiePair[0].trim();
+                if (cookieKey === name) {
+                        return decodeURIComponent(cookiePair[1]);
+                }
+        }
+        return null;
+}
+
+export default axiosInstance;
